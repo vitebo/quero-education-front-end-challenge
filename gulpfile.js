94d75e270c8339b9f87dd367a1ca8d869fb67316
vitebo <vitebo51@gmail.com>
2018-10-11 20:39:10 -0300

    adicionando um servidor estático

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const connect = require('gulp-connect')
 
 gulp.task('styles', () => {
   gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css/'))
 })
+
+gulp.task('serve', () =>
+  connect.server({
+    root: './dist/',
+    livereload: true
+  })
+)
