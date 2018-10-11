50c7ea41009d508d4901ec967ca3cb602144d56a
vitebo <vitebo51@gmail.com>
2018-10-11 20:05:46 -0300

    adicionando sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,8 @@
 const gulp = require('gulp')
-console.log(gulp)
+const sass = require('gulp-sass')
+
+gulp.task('styles', () => {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'))
+})
