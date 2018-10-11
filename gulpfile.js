1d51b546de211eecfbc1a3291a271813a36eb6db
vitebo <vitebo51@gmail.com>
2018-10-11 20:53:24 -0300

    criando task para eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const connect = require('gulp-connect')
+const eslint = require('gulp-eslint')
 
 gulp.task('styles', () => {
   gulp.src('./src/sass/**/*.scss')
@@ -11,6 +12,15 @@ gulp.task('styles', () => {
 gulp.task('html', () => {
   gulp.src('./src/index.html')
     .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('eslint', () => {
+  gulp.src(['./src/js/**/*.js'])
+    .pipe(eslint({
+      configFilea: './.eslintrc.yml'
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
 
 gulp.task('serve', () =>
