45a3e94d452c54e9be99b8c2fcb0e7b38ecab799
vitebo <vitebo51@gmail.com>
2018-10-17 00:13:31 -0300

    Update 2018-10-17T03:13:25.189Z

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,24 @@
   const $btnToggleHelpMenu = doc.querySelector('[data-js="help-menu-toggle"]')
   const $mainMenu = doc.querySelector('[data-js="main-menu"]')
   const $helpMenu = doc.querySelector('[data-js="help-menu"]')
+  const $taskbar = doc.querySelector('[data-js="taskbar"]')
+
   $btnToggleMainMenu.addEventListener('click', e => $mainMenu.classList.toggle('is-expanded'))
   $btnToggleHelpMenu.addEventListener('click', e => {
     $helpMenu.classList.toggle('is-expanded')
     doc.body.classList.toggle('u-hidden-scroll-mobile')
+  })
+
+  let lastScrollY = 0
+  doc.addEventListener('scroll', () => {
+    if (win.scrollY < lastScrollY) {
+      $taskbar.classList.remove('is-hidden')
+      lastScrollY = win.scrollY
+      return
+    }
+    if (win.scrollY > 56) {
+      $taskbar.classList.add('is-hidden')
+      lastScrollY = win.scrollY
+    }
   })
 })(window, document)
