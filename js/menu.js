521287a630c84c29ec3ec4936f0f035a87b3db1b
vitebo <vitebo51@gmail.com>
2018-10-17 20:52:14 -0300

    Update 2018-10-17T23:52:09.740Z

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,29 @@
+"use strict";
+
 (function (win, doc) {
-  const $btnToggleMainMenu = doc.querySelector('[data-js="main-menu-toggle"]')
-  const $btnToggleHelpMenu = doc.querySelector('[data-js="help-menu-toggle"]')
-  const $mainMenu = doc.querySelector('[data-js="main-menu"]')
-  const $helpMenu = doc.querySelector('[data-js="help-menu"]')
-  const $taskbar = doc.querySelector('[data-js="taskbar"]')
-
-  $btnToggleMainMenu.addEventListener('click', e => $mainMenu.classList.toggle('is-expanded'))
-  $btnToggleHelpMenu.addEventListener('click', e => {
-    $helpMenu.classList.toggle('is-expanded')
-    doc.body.classList.toggle('u-hidden-scroll-mobile')
-  })
-
-  let lastScrollY = 0
-  doc.addEventListener('scroll', () => {
+  var $btnToggleMainMenu = doc.querySelector('[data-js="main-menu-toggle"]');
+  var $btnToggleHelpMenu = doc.querySelector('[data-js="help-menu-toggle"]');
+  var $mainMenu = doc.querySelector('[data-js="main-menu"]');
+  var $helpMenu = doc.querySelector('[data-js="help-menu"]');
+  var $taskbar = doc.querySelector('[data-js="taskbar"]');
+  $btnToggleMainMenu.addEventListener('click', function (e) {
+    return $mainMenu.classList.toggle('is-expanded');
+  });
+  $btnToggleHelpMenu.addEventListener('click', function (e) {
+    $helpMenu.classList.toggle('is-expanded');
+    doc.body.classList.toggle('u-hidden-scroll-mobile');
+  });
+  var lastScrollY = 0;
+  doc.addEventListener('scroll', function () {
     if (win.scrollY < lastScrollY) {
-      $taskbar.classList.remove('is-hidden')
-      lastScrollY = win.scrollY
-      return
+      $taskbar.classList.remove('is-hidden');
+      lastScrollY = win.scrollY;
+      return;
     }
+
     if (win.scrollY > 56) {
-      $taskbar.classList.add('is-hidden')
-      lastScrollY = win.scrollY
+      $taskbar.classList.add('is-hidden');
+      lastScrollY = win.scrollY;
     }
-  })
-})(window, document)
+  });
+})(window, document);
